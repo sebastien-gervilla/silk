f27f4546e0bf11dc67bfda6a57c3bd92142263f1
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2024-10-31 22:57:41 +0100

    mod: Moved compiler to backend

---
+++ src/backend/mod.rs
@@ -0,0 +1,6 @@
+pub mod value;
+pub mod object;
+pub mod bytecode;
+pub mod debug;
+pub mod compiler;
+pub mod vm;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod token;
-pub mod lexer;
+pub mod backend;
 pub mod ast;
 pub mod parser;
-pub mod compiler;
 pub mod typecheck;
